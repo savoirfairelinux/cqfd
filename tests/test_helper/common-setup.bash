aee083740ca0e4c95ea5d8b19570ea5c99e3ef18
Florent Allard <florent.allard@savoirfairelinux.com>
2025-11-17 16:00:04 -0500

    tests: move common setup to setup_suite
    This allows for more factorization of the code

--- tests/test_helper/common-setup.bash
+++ tests/test_helper/common-setup.bash
@@ -3,11 +3,4 @@
 _common_setup() {
     load 'test_helper/bats-support/load'
     load 'test_helper/bats-assert/load'
-    # get the containing directory of this file
-    # use $BATS_TEST_FILENAME instead of ${BASH_SOURCE[0]} or $0,
-    # as those will point to the bats executable's location or the preprocessed file respectively
-    PROJECT_ROOT="$( cd "$( dirname "$BATS_TEST_FILENAME" )/.." >/dev/null 2>&1 && pwd )"
-    # make executables in src/ visible to PATH
-    PATH="$PROJECT_ROOT:$PATH"
-    cd "$BATS_SUITE_TMPDIR/" || exit 1
 }
